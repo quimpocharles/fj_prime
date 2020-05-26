4e8806efcf8366ebaa0793a5a91022ded4ec1564
Charles Quimpo <charles.quimpo@tuitt.com>
2020-05-26 12:32:19 +0800

    v0.3

--- src/index.js
+++ src/index.js
@@ -42,10 +42,7 @@ const Logout = () => {
 var hist = createBrowserHistory();
 
 const client = new ApolloClient({
-  uri: "https://fjserver.herokuapp.com/graphql",
-  fetchOptions: {
-    mode: "no-cors",
-  },
+  uri: "http://localhost:4000/graphql",
 });
 
 const EcommercePage = Loadable({
